4140966f6d142ea37d7f63a5f1690c329999b5ca
ninj0x <ninj0x@gmail.com>
2013-01-13 15:24:19 -0800

    abstracted test client's hostname config to an argument

--- client/rmi/TestClient.java
+++ client/rmi/TestClient.java
@@ -9,29 +9,31 @@ public class TestClient implements Runnable{
 	String myName;
 
 	public static void main(String[] args) {
-		TestClient client = new TestClient();
+		String hostname = "localhost";
+		if (args.length >= 1) {
+			hostname = args[0];
+		}
+		TestClient client = new TestClient(hostname);
 		client.run();
 	}
 
-	private TestClient() {
+	private TestClient(String hostname) {
 		Random r = new Random();
 		char c = (char)(r.nextInt(26) + 'A');
 		myName = "TestClient" + c;
-		pheme = new Pheme("localhost");
+		pheme = new Pheme(hostname);
 	}
 
 	@Override
 	public void run() {
 		int count = 0;
-		while (true){
-//			pheme.log(myName, "info", "Will the real TestClient please stand up?");
+		while (count < 1000){
 			try {
 				Thread.sleep(10);
 				pheme.log(myName, "info", "Will the real TestClient please stand up? " + count);
 				System.out.println("sent log " + count);
 				count++;
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
